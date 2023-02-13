3f354f0b36f1efe678325f93c879e1f2060f619c
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-02-13 14:08:12 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import About from "./components/About";
+import Header from "./components/Header";
+import Boost from "./components/Boost";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <About/>
+      <Boost/>
+      <Footer/>
+
  
     </div>
   );
---
+++ src/components/Boost.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Boost.module.scss";
+
+const Simplify = () => {
+  return (
+    <div className={styles.simplify}>
+        <h2>Boost your link today.</h2>
+        <button className={styles.button}>Get Started</button>
+    </div>
+  )
+}
+
+export default Simplify
