d5b6166bf1b4a495e2590f3dbb442056bea1ef81
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-02-13 18:26:39 +0200

    3th commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Boost from "./components/Boost";
 import Footer from "./components/Footer";
+import ShortenLink from "./components/ShortenLink";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <About/>
+      <ShortenLink/>
       <Boost/>
       <Footer/>
+   
 
  
     </div>
